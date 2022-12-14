dbee6ab3be76630e8dd0647546311b25e72d3778
enzonatale <104118019+enzonatale@users.noreply.github.com>
2022-12-13 21:53:41 -0300

    Add files via upload

---
+++ js/inicia.js
@@ -0,0 +1,13 @@
+
+  $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+      
+  
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
